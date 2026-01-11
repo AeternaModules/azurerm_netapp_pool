45e9a70b2eb6974f12abb869757d419b7e17c938
Daniel Allen <dan@aeternagroup.net>
2026-01-11 16:50:34 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "netapp_pools" {
+  description = <<EOT
+Map of netapp_pools, attributes below
+Required:
+    - account_name
+    - location
+    - name
+    - resource_group_name
+    - service_level
+    - size_in_tb
+Optional:
+    - cool_access_enabled
+    - custom_throughput_mibps
+    - encryption_type
+    - qos_type
+    - tags
+EOT
+
+  type = map(object({
+    account_name            = string
+    location                = string
+    name                    = string
+    resource_group_name     = string
+    service_level           = string
+    size_in_tb              = number
+    cool_access_enabled     = optional(bool, false)
+    custom_throughput_mibps = optional(number)
+    encryption_type         = optional(string, "Single")
+    qos_type                = optional(string, "Auto")
+    tags                    = optional(map(string))
+  }))
+}
+
